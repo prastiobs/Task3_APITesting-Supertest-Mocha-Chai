def9bf4c8ede99b6eabf127d6662501eede3f933
Prastio <prastiobagusindra@gmail.com>
2024-05-01 20:25:47 +0700

    add delete api test

--- test/deleteUser.test.js
+++ test/deleteUser.test.js
@@ -1,20 +1,18 @@
 import request from 'supertest';
 import { expect } from 'chai';
 
-describe('Delete API tests using supertest', () => {
+describe('Get API tests using supertest', () => {
 	const baseurl = 'https://kasir-api.belajarqa.com';
-	it('Delete User', (done) => {
+	it('Get User Detail', (done) => {
 		request(baseurl)
-		.get('/users/05ad66ce-13b5-4522-9544-e159555ad597')
+		.delete('/users/05ad66ce-13b5-4522-9544-e159555ad597')
 		.set('Accept', 'application/json')
 		.set('Content-Type', 'application/json')
 		.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwNWYyZWMyLTZjOGYtNGEwZi1iMDM0LTE5NzhkYjM2ZDJkNyIsImNvbXBhbnlJZCI6Ijg2NzM1M2E3LTBkNjYtNDg4OS1hYzc0LWE5YWE4NDk2MjI2YSIsImlhdCI6MTcxNDU2OTUzN30.Tr-ImUrkkPy3vIzgDOhlpSV6W0-esxuc3pF4qqG4T9Q')
 		.end(function (err, res) {
 			expect(res.statusCode).to.be.equal(200);
 			expect(res.body.status).to.be.equal('success');
-			expect(res.body.data.user.name).to.be.equal('kasir-serbaguna');
-			expect(res.body.data.user.email).to.be.equal('user@example.com');
-			expect(res.body.data.user.role).to.be.equal('kasir');
+			expect(res.body.message).to.be.equal('User berhasil dihapus');
 			done();
 		});
 	});
